dd6529982949e6c7e00ebc633d959ca0020aa00b
unknown <khawladahech6@gmail.com>
2024-03-29 12:40:44 +0100

    update storage

--- main.js
+++ main.js
@@ -1,6 +1,15 @@
 const notesContainer= document.querySelector(".notes-container");
 const createBtn = document.querySelector(".btn");
-let notes = document.querySelectorAll("input-box");
+let notes = document.querySelectorAll(".input-box");
+
+function showNotes(){
+  notesContainer.innerHTML = localStorage.getItem('notes');
+}
+showNotes();
+ 
+function updateStorage(){
+  localStorage.setItem("notes",notesContainer.innerHTML);
+}
 
 createBtn.addEventListener("click",()=>{
   let inputBox = document.createElement("p")
@@ -14,6 +23,20 @@ createBtn.addEventListener("click",()=>{
 notesContainer.addEventListener("click",function(e){
   if(e.target.tagName==="IMG"){
     e.target.parentElement.remove();
-
+    updateStorage();
   }
-})
+  else if(e.target.tagName==="p"){
+    notes=document.querySelectorAll(".input-box");
+    notes.forEach(nt=>{
+      nt.onkeyup=function(){
+        updateStorage();
+      }
+    })
+  }
+});
+document.addEventListener("keydown",event =>{
+  if(event.key === "Enter"){
+    document.execCommand("insertLineBreak");
+    event.preventDefault();
+  }
+});
